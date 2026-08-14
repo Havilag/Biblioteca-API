ee85aad3e4b71291fe7db369ce9d929bfb3ad796
Havilag <havilag1505@gmail.com>
2026-08-13 23:48:34 -0500

    actualización de setting

--- biblioteca_api/settings.py
+++ biblioteca_api/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 AUTH_USER_MODEL = 'users.User'
 
