ddef76dc7687a2f2ceaa3e90eb96389819c30fdf
Havilag <havilag1505@gmail.com>
2026-08-14 00:05:58 -0500

    script para crear superusuario

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@gmail.com', 'Admin12345*')
+    print('Superusuario admin creado con éxito')
+"
